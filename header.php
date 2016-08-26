53bbe0cc855ed28258ada089d4e48c8bc4c20b94
etucker210 <etucker210@users.noreply.github.com>
2016-08-26 12:22:06 -0400

    Bootstrap and jQuery
    Addition of bootstrap and jquery to a header file.

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+    <script src="../jquery/jquery.min.js"></script>
+    <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </head>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+    $title = "Welcome";
+    include_once 'header.php'; ?>
+</html>
