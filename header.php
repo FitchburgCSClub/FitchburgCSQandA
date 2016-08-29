8c3c1eaccaf7efafa962ee50f2b89782ba4f1616
Elijah Tucker <etucker210@users.noreply.github.com>
2016-08-29 19:50:45 -0400

    Log in popover stil needs functions

--- header.php
+++ header.php
@@ -1,4 +1,31 @@
-<?php $websiteName = "Fitchburg State CS Q and A"; ?>
+<?php $websiteName = "Fitchburg State CS Q and A";
+$userError = $passError = "";
+$userName = $password = "";
+$valid = true;
+
+if ( $_SERVER["REQUEST_METHOD"] == "POST" )
+{
+
+  if (isset($_POST['submit'])) {
+    if ( $POST['submit'] == 'Login' ) {
+      echo "login";
+    }
+    elseif ($POST['submit'] == 'Register') {
+      echo "register";
+    }
+
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+ ?>
   <head>
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
@@ -11,10 +38,32 @@
         <a class="navbar-brand" href="index.php"><?php echo $websiteName; ?></a>
       </div>
       <ul class="nav navbar-nav">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+        <li><a href="#" title="Sign Up" data-toggle="popover" data-placement="bottom"
+               data-content=
+               "<form method='post' role='form' class='form-horizontal'
+                  action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+                   <div class='form-group'>
+                     <label class='control-label' for='usedrName'>User Name:</label>
+                     <div>
+                       <input type='text' name='userName' id='userName' class='form-control'/>
+                     </div>
+                   </div>
+                   <div class='form-group'>
+                     <label class='control-label' for='password'>Password:</label>
+                     <div>
+                       <input type='passnword' name='password' id='password' class='form-control'/>
+                     </div>
+                   </div>
+                   <div class='form-group'>
+                     <input type='submit' value='Register' class='btn btn-success'/>
+                   </div>
+               </form>
+               ">
+               <span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
         <li><a href="#" title="Login" data-toggle="popover" data-placement="bottom"
           data-content=
-          "<form method='post' role='form' class='form-horizontal'>
+          "<form method='post' role='form' class='form-horizontal'
+            action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
               <div class='form-group'>
                 <label class='control-label' for='usedrName'>User Name:</label>
                 <div>
@@ -28,7 +77,7 @@
                 </div>
               </div>
               <div class='form-group'>
-                <input type='submit' value='Submit' class='btn btn-success'/>
+                <input type='submit' value='Login' class='btn btn-success'/>
               </div>
           </form>
             ">
@@ -39,7 +88,8 @@
   <script>
     $(document).ready(function(){
       $('[data-toggle="popover"]').popover({
-        html:true
+        html: true,
+        container: 'nav'
       });
     });
   </script>
