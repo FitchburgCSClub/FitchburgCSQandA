bae295b231cc107c3159a6f24e74a6130e84d17f
Elijah Tucker <etucker210@users.noreply.github.com>
2016-08-29 16:19:46 -0400

    move bootstrap

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
 <html>
   <head>
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
-    <script src="../jquery/jquery.min.js"></script>
-    <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <script src="/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </head>
   <nav class="navbar navbar-inverse">
     <div class="container-fluid">
